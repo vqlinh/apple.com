43c9cfc6ba3b8aa544798e13cf166e1e81beea08
unknown <vuongquanglinh1234@gmail.com>
2022-09-21 00:14:58 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const searchButton = document.querySelector('nav .desktop-nav .link-search')
+const closeButton = document.querySelector(' .link-close')
+const desktopNav = document.querySelector('.desktop-nav ')
+const searchContainer = document.querySelector('.search-container ')
+const overlay = document.querySelector('.overlay ')
+
+searchButton.addEventListener("click", () => {
+    desktopNav.classList.add('hide');
+    searchContainer.classList.remove('hide')
+    overlay.classList.add('show');
+
+
+})
+closeButton.addEventListener("click", () => {
+    desktopNav.classList.remove('hide');
+    searchContainer.classList.add('hide')
+    overlay.classList.remove('show');
+
+
+})
+overlay.addEventListener("click", () => {
+    desktopNav.classList.remove('hide');
+    searchContainer.classList.add('hide')
+    overlay.classList.remove('show');
+
+
+})
+
+
